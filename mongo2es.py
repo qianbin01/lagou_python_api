3550e663a0c84d2c5deed9ec526b947f43f4d506
qb <qianbin@mike-mary.onaliyun.com>
2018-09-07 16:49:38 +0800

    es抽象查询方法

--- mongo2es.py
+++ mongo2es.py
@@ -1,13 +1,13 @@
 from elasticsearch import Elasticsearch
-from models import article
+from models import recruit
 import config
 
 es = Elasticsearch(config.ES_HOST, timeout=180)
 es.indices.delete(index='qb-lagou', ignore=[400, 404])
-for item in article.find():
-    item['aid'] = str(item['_id'])
+for item in recruit.find():
+    item['rid'] = str(item['_id'])
     item.pop('_id')
     es.index(index="qb-lagou",
-             doc_type="article",
-             id=item['aid'],
+             doc_type="recruit",
+             id=item['rid'],
              body=item)
